2f9d04e05df7a251388857efd36be2a09a121ad3
Dan Rauch <drauch@Dans-iMac.local>
2018-06-22 21:01:00 -0500

    init 6.22.2018

---
+++ README.md
@@ -0,0 +1,11 @@
+# MedTrack-dev
+
+MedTrack is a personal web client used by medical students to track shadowing hours. This is the development client. 
+
+## Used Libraries
+
+* 
+
+# Collaborators
+
+* Dan Rauch
---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const http = require('http')
+const port = process.env.PORT || 4000 
+const path = require('path')
+const request = require('request')
+const json = require('json');
+const session = require('express-session')
+const express = require('express')
+const app = express()
+
+// Generic Express config
+app.set('port', port) 
+app.set('views', 'views')
+app.use(express.static(__dirname))
+app.use(session({ resave: false, saveUninitialized: false, secret: 'smith' }))
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'))
+})
+
+app.get('/', (req, res) => {
+    res.render('pages/index.ejs')
+})
